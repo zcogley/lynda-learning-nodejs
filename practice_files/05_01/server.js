bcf20e6f69fc88e6d0ab469449c164b316c7bfed
Zac Cogley <zaccogley@gmail.com>
2018-04-03 11:35:40 -0500

    complete course

--- practice_files/05_01/server.js
+++ practice_files/05_01/server.js
@@ -3,28 +3,65 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+mongoose.Promise = Promise
 
-app.get('/messages', (req, res) => {
-    res.send(messages)
+var dbURL = 'mongodb://user:user@ds113795.mlab.com:13795/learning-node'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.post('/messages', async (req, res) => {
+    try {
+
+        var message = new Message(req.body)
+    
+        var savedMessage = await message.save()
+
+        console.log('saved')
+
+        var censored = await Message.findOne({message: 'badword'})
+
+        if(censored) 
+            await Message.remove({_id: censored.id})
+        else 
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
+    }
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbURL, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
